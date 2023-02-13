2bb3c5bdff2c3cb71d173dbcf88580e79553db8d
Chuanwise <chuanwise@qq.com>
2023-02-13 11:30:26 +0800

    update comments in BotInternalAPI

--- xiaoming-core-api/src/main/java/cn/codethink/xiaoming/annotation/BotInternalAPI.java
+++ xiaoming-core-api/src/main/java/cn/codethink/xiaoming/annotation/BotInternalAPI.java
@@ -3,9 +3,9 @@ package cn.codethink.xiaoming.annotation;
 import java.lang.annotation.*;
 
 /**
- * <h1>Bot 内部 API</h1>
+ * <h1>机器人内部 API</h1>
  *
- * <p>Bot 内部 API 是不应该被小明核心组件开发者之外的人调用的 API。这些 API 可能在任何时候被改动
+ * <p>机器人内部 API 是不应该被小明核心组件开发者之外的人调用的 API。这些 API 可能在任何时候被改动
  * 而不提前通知。除非你正在开发小明核心组件，否则 <b>不要在你代码中的任何位置调用这些 API</b>。</p>
  *
  * <ui>
