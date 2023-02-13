61a568c002a924e6fc7f3ee638c88d15999a71ef
Chuanwise <chuanwise@qq.com>
2023-02-13 11:30:42 +0800

    Add bot related interfaces `Bot` and `BotObject`

---
+++ xiaoming-core-api/src/main/java/cn/codethink/xiaoming/common/BotObject.java
@@ -0,0 +1,20 @@
+package cn.codethink.xiaoming.common;
+
+import cn.codethink.xiaoming.Bot;
+
+/**
+ * <h1>机器人对象</h1>
+ *
+ * <p>对应一个机器人的对象</p>
+ *
+ * @author Chuanwise
+ */
+public interface BotObject {
+    
+    /**
+     * 获取机器人
+     *
+     * @return 机器人
+     */
+    Bot getBot();
+}
