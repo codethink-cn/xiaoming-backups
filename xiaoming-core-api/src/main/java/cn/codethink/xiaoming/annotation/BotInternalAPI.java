8add7c5bd2f2ef922beac16089f0781cb57a1a87
Chuanwise <chuanwise@qq.com>
2023-03-14 10:04:07 +0800

    update descriptor annotations

--- xiaoming-core-api/src/main/java/cn/codethink/xiaoming/annotation/BotInternalAPI.java
+++ xiaoming-core-api/src/main/java/cn/codethink/xiaoming/annotation/BotInternalAPI.java
@@ -8,16 +8,9 @@ import java.lang.annotation.*;
  * <p>机器人内部 API 是不应该被小明核心组件开发者之外的人调用的 API。这些 API 可能在任何时候被改动
  * 而不提前通知。除非你正在开发小明核心组件，否则 <b>不要在你代码中的任何位置调用这些 API</b>。</p>
  *
- * <ui>
- *     <li>当一个方法被该注解修饰，意味着该方法是内部 API。</li>
- *     <li>当一个类或接口被该注解修饰，意味着该类或接口及其所有子类都是内部 API。</li>
- *     <li>当一个属性被该注解修饰，意味着该属性相关的方法都是内部 API。</li>
- * </ui>
- *
  * @author Chuanwise
  */
 @Documented
-@Target({ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.TYPE, ElementType.FIELD})
 @Retention(RetentionPolicy.SOURCE)
 public @interface BotInternalAPI {
 }
